ac338d8cdb83a595cdc2f3286206971cdf722aaa
Teal Pershing <tjpershing@ucdavis.edu>
2020-03-23 12:08:42 -0700

    Modified Profile likelihood to use the background multiplicity distribution itself rather than model it as Poissonian

--- ANNIENtupleAnalysis/lib/ProfileLikelihoodBuilder.py
+++ ANNIENtupleAnalysis/lib/ProfileLikelihoodBuilder.py
@@ -18,7 +18,7 @@ class ProfileLikelihoodBuilder(object):
     def SetBkgMeanUnc(self,mean_unc):
         self.bkg_pois_mean_unc = mean_unc
 
-    def BuildLikelihoodProfile(self,ProfileVariable,SignalDistribution,SignalDistribution_unc,randShoots):
+    def BuildLikelihoodProfile(self,ProfileVariable,SignalDistribution,SignalDistribution_unc,randShoots,BackgroundDistribution):
         '''
         Returns a Chi-squared profile given the input profile variables, the background mean neutron rate
         defined with the SetBkgMean method, and a signal distribution.
@@ -28,7 +28,7 @@ class ProfileLikelihoodBuilder(object):
             return None
         ChiSquares = []
         for var in ProfileVariable:
-            MCProfile = self.BuildMCProfile(var,SignalDistribution,randShoots)
+            MCProfile = self.BuildMCProfileBkgDist(var,SignalDistribution,BackgroundDistribution,randShoots)
             ChiSquare = self.CalcChiSquare(MCProfile,SignalDistribution,SignalDistribution_unc)
             ChiSquares.append(ChiSquare)
         return ChiSquares
@@ -39,21 +39,32 @@ class ProfileLikelihoodBuilder(object):
         self.bkg_pois_mean, build a Monte Carlo-based data distribution.
         '''
         Profile = np.zeros(len(SignalDistribution))
-        #for j in range(randShoots):
-        #    neutron_num = 0
-        #    anum = np.random.random()
-        #    if (anum <= variable):
-        #        neutron_num+=1
-        #    Bkg_shot = np.random.poisson(self.bkg_pois_mean,1)
-        #    neutron_num+=Bkg_shot
-        #    if(neutron_num<len(SignalDistribution)):
-        #        Profile[neutron_num]+=1
+        poisson_means = np.random.normal(self.bkg_pois_mean,self.bkg_pois_mean_unc,randShoots)
+        poisson_means = poisson_means[np.where(poisson_means>0)[0]]
+        randShoots = len(poisson_means)
         neutron_counts = np.zeros(randShoots)
         randos = np.random.random(randShoots)
         saw_neutrons = np.where(randos<=variable)[0]
         neutron_counts[saw_neutrons]+=1
-        Bkg_shoots = np.random.poisson(self.bkg_pois_mean,randShoots)
+        Bkg_shoots = np.random.poisson(poisson_means)
+        #Bkg_shoots = np.random.poisson(self.bkg_pois_mean,randShoots)
         neutron_counts = neutron_counts + Bkg_shoots
+        Profile,Profile_edges = np.histogram(neutron_counts,range=(0,len(SignalDistribution)),bins=len(SignalDistribution))
+        Profile_normed = Profile/np.sum(Profile)
+        return Profile_normed
+
+    def BuildMCProfileBkgDist(self,variable,SignalDistribution,BkgDistribution,randShoots):
+        '''
+        Given the probability of returning a 1 in the signal distribution and the defined background mean in
+        self.bkg_pois_mean, build a Monte Carlo-based data distribution.
+        '''
+        Profile = np.zeros(len(SignalDistribution))
+        Bin_values = np.arange(0,len(SignalDistribution),1)
+        neutron_counts = np.zeros(randShoots)
+        neutron_counts += np.random.choice(Bin_values, randShoots,p=BkgDistribution)
+        randos = np.random.random(randShoots)
+        saw_neutrons = np.where(randos<=variable)[0]
+        neutron_counts[saw_neutrons]+=1
         Profile,Profile_edges = np.histogram(neutron_counts,range=(0,len(SignalDistribution)),bins=len(SignalDistribution))
         Profile_normed = Profile/np.sum(Profile)
         return Profile_normed
